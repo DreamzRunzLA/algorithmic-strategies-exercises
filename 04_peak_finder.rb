6c91b0a73a33e10fef56679e467303495e9ea9a8
DreamzRunzLA <salmaan.a.qadir@gmail.com>
2019-07-17 23:18:33 -0600

    Finsihed 1-3, unsure of 4, need to work on 5 and plan it out on paper before attempting to solve on computer

--- 04_peak_finder.rb
+++ 04_peak_finder.rb
@@ -2,7 +2,24 @@
 # The method should return an array containing all of "peaks" of the array.
 # An element is considered a "peak" if it is greater than both it's left and right neighbor.
 # The first or last element of the array is considered a "peak" if it is greater than it's one neighbor.
+def peak_finder(arr)
+    ans = []
+    if arr[0] > arr[1]
+        ans << arr[0]
+    end
 
+    if arr[-1] > arr[-2]
+        ans << arr[-1]
+    end
+
+    (1...arr.length - 1).each do |i|
+        if arr[i] > arr[i-1] && arr[i] > arr[i+1]
+            ans << arr[i]
+        end
+    end
+
+    ans
+end
 
 p peak_finder([1, 3, 5, 4])         # => [5]
 p peak_finder([4, 2, 3, 6, 10])     # => [4, 10]
--- 05_compress_str.rb
+++ 05_compress_str.rb
@@ -1,7 +1,28 @@
 # Write a method, compress_str(str), that accepts a string as an arg.
 # The method should return a new str where streaks of consecutive characters are compressed.
 # For example "aaabbc" is compressed to "3a2bc".
+def compress_str(str)
+    ansArr = []
+    strArr = str.split('')
 
+    while strArr.length > 1 do
+        counter = 1
+        eleToAdd = ''
+        (0...strArr.length - 1).each do |i|
+            if strArr[i] == strArr[i+1]
+                counter += 1
+                eleToAdd = strArr[i]
+                strArr.delete_at(i)
+            else
+                ansArr << strArr[i]
+                strArr.delete_at(i)
+            end
+        end
+        ansArr << counter.to_s
+        ansArr << eleToAdd
+    end
+    ansArr.join('')
+end
 
 p compress_str("aaabbc")        # => "3a2bc"
 p compress_str("xxyyyyzz")      # => "2x4y2z"
